50046f8894a5d1d68b57bb86fe7099a4d8940ab9
Pramod-HaHa <135501469+Pramod-HaHa@users.noreply.github.com>
2024-03-14 08:16:53 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,11 @@
-/// Initialize Firebase (ADD YOUR OWN DATA)
-const firebaseConfig = {
-  apiKey: "AIzaSyDhXc8N9sBOCbYM9jmJ847_B6-lO6FW__g",
-  authDomain: "location-44d93.firebaseapp.com",
-  projectId: "location-44d93",
-  storageBucket: "location-44d93.appspot.com",
-  messagingSenderId: "166329516075",
-  appId: "1:166329516075:web:0d765c97cc8bdf44846a6e",
-  measurementId: "G-PJW4B3F24G",
-    
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwxrr9gY3hHl86SuG0uv8nQa0NJHnmqFYqzCzrqM0w4wWDuE-B--j9r9iwch5FHZK6o/exec'
 
-  };
+const form = document.forms['contact-form']
 
-firebase.initializeApp(firebaseConfig);
-
-// Reference messages collection
-var messagesRef = firebase.database().ref('messages');
-
-// Listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-// Submit form
-function submitForm(e){
-  e.preventDefault();
-
-  // Get values
-  var name = getInputVal('name');
-  var company = getInputVal('company');
-  var email = getInputVal('email');
-  var phone = getInputVal('phone');
-  var message = getInputVal('message');
-
-  // Save message
-  saveMessage(name, company, email, phone, message);
-
-  // Show alert
-  document.querySelector('.alert').style.display = 'block';
-
-  // Hide alert after 3 seconds
-  setTimeout(function(){
-    document.querySelector('.alert').style.display = 'none';
-  },3000);
-
-  // Clear form
-  document.getElementById('contactForm').reset();
-}
-
-// Function to get get form values
-function getInputVal(id){
-  return document.getElementById(id).value;
-}
-
-// Save message to firebase
-function saveMessage(name, company, email, phone, message){
-  var newMessageRef = messagesRef.push();
-  newMessageRef.set({
-    name: name,
-    company:company,
-    email:email,
-    phone:phone,
-    message:message
-  });
-}
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
